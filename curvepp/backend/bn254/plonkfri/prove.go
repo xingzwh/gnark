c991401d0ba6cfdcce5273f89428cc39c117ea5f
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 19:13:31 +0800

    Update prove.go

--- curvepp/backend/bn254/plonkfri/prove.go
+++ curvepp/backend/bn254/plonkfri/prove.go
@@ -26,13 +26,13 @@ import (
 
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr/fft"
 
-	"github.com/consensys/gnark/constraint/bn254"
+	"github.com/xingzwh/gnark/constraint/bn254"
 
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr/fri"
 
 	fiatshamir "github.com/consensys/gnark-crypto/fiat-shamir"
-	"github.com/consensys/gnark/backend"
-	"github.com/consensys/gnark/internal/utils"
+	"github.com/xingzwh/gnark/backend"
+	"github.com/xingzwh/gnark/curvepp/utils"
 )
 
 type Proof struct {
