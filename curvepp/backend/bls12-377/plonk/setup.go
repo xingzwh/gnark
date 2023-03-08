5276beca50bda206f2d6d38e48f1fdd1fa73e57f
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 17:03:17 +0800

    Update setup.go

--- curvepp/backend/bls12-377/plonk/setup.go
+++ curvepp/backend/bls12-377/plonk/setup.go
@@ -22,7 +22,7 @@ import (
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr/fft"
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr/iop"
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr/kzg"
-	"github.com/consensys/gnark/constraint/bls12-377"
+	"github.com/xingzwh/gnark/constraint/bls12-377"
 
 	kzgg "github.com/consensys/gnark-crypto/kzg"
 )
