1b2deb96c49fca9e372c37ce2bdd08e258b0c761
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 16:11:03 +0800

    Update pairing.go

--- std/algebra/sw_bls24315/pairing.go
+++ std/algebra/sw_bls24315/pairing.go
@@ -21,8 +21,8 @@ import (
 	"math/big"
 
 	"github.com/consensys/gnark-crypto/ecc"
-	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/std/algebra/fields_bls24315"
+	"github.com/xingzwh/gnark/frontend"
+	"github.com/xingzwh/gnark/std/algebra/fields_bls24315"
 )
 
 // GT target group of the pairing
