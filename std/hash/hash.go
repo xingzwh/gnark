8d88eb160646533007a3e3070dcb2c414ba1fc36
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 16:24:32 +0800

    Update hash.go

--- std/hash/hash.go
+++ std/hash/hash.go
@@ -17,7 +17,7 @@ limitations under the License.
 // Package hash provides an interface that hash functions (as gadget) should implement.
 package hash
 
-import "github.com/consensys/gnark/frontend"
+import "github.com/xingzwh/gnark/frontend"
 
 type Hash interface {
 
