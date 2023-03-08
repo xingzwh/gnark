b507db6d2b4f7e717a0c2c34cb6b6edc86488177
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 17:06:34 +0800

    Update verify.go

--- curvepp/backend/bls12-381/groth16/verify.go
+++ curvepp/backend/bls12-381/groth16/verify.go
@@ -22,7 +22,7 @@ import (
 	"github.com/consensys/gnark-crypto/ecc"
 	curve "github.com/consensys/gnark-crypto/ecc/bls12-381"
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
-	"github.com/consensys/gnark/logger"
+	"github.com/xingzwh/gnark/logger"
 	"io"
 	"math/big"
 	"time"
