1ee1805ad115a13ca062257db543447ec60088e0
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 16:08:32 +0800

    Update g2.go

--- std/algebra/sw_bls12377/g2.go
+++ std/algebra/sw_bls12377/g2.go
@@ -21,9 +21,9 @@ import (
 
 	"github.com/consensys/gnark-crypto/ecc"
 	bls12377 "github.com/consensys/gnark-crypto/ecc/bls12-377"
-	"github.com/consensys/gnark/backend/hint"
-	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/std/algebra/fields_bls12377"
+	"github.com/xingzwh/gnark/backend/hint"
+	"github.com/xingzwh/gnark/frontend"
+	"github.com/xingzwh/gnark/std/algebra/fields_bls12377"
 )
 
 // G2Jac point in Jacobian coords
