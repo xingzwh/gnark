145fc940bc5d3605123f32d98b15a3d4dcb76766
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 15:44:25 +0800

    Update api.go

--- frontend/api.go
+++ frontend/api.go
@@ -19,7 +19,7 @@ package frontend
 import (
 	"math/big"
 
-	"github.com/consensys/gnark/backend/hint"
+	"github.com/xingzwh/gnark/backend/hint"
 )
 
 // API represents the available functions to circuit developers
