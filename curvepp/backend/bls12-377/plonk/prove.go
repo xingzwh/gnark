aa870817e3247060a16be666aa794aef44aa89bc
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 17:02:54 +0800

    Update prove.go

--- curvepp/backend/bls12-377/plonk/prove.go
+++ curvepp/backend/bls12-377/plonk/prove.go
@@ -32,12 +32,12 @@ import (
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr/fft"
 
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr/iop"
-	"github.com/consensys/gnark/constraint/bls12-377"
+	"github.com/xingzwh/gnark/constraint/bls12-377"
 
 	"github.com/consensys/gnark-crypto/fiat-shamir"
-	"github.com/consensys/gnark/backend"
-	"github.com/consensys/gnark/internal/utils"
-	"github.com/consensys/gnark/logger"
+	"github.com/xingzwh/gnark/backend"
+	"github.com/xingzwh/gnark/curvepp/utils"
+	"github.com/xingzwh/gnark/logger"
 )
 
 type Proof struct {
