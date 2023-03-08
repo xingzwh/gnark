7e138ee7fc3b49ff9b9892b46fea042fad03925e
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 16:19:41 +0800

    Update settings.go

--- std/fiat-shamir/settings.go
+++ std/fiat-shamir/settings.go
@@ -1,8 +1,8 @@
 package fiatshamir
 
 import (
-	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/std/hash"
+	"github.com/xingzwh/gnark/frontend"
+	"github.com/xingzwh/gnark/std/hash"
 )
 
 type Settings struct {
