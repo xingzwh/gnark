9288161e4e884dc10e19bb61d6dbadd5eb2c3728
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-03-08 16:04:35 +0800

    Update e12.go

--- std/algebra/fields_bls12377/e12.go
+++ std/algebra/fields_bls12377/e12.go
@@ -20,8 +20,8 @@ import (
 	"math/big"
 
 	bls12377 "github.com/consensys/gnark-crypto/ecc/bls12-377"
-	"github.com/consensys/gnark/backend/hint"
-	"github.com/consensys/gnark/frontend"
+	"github.com/xingzwh/gnark/backend/hint"
+	"github.com/xingzwh/gnark/frontend"
 )
 
 // Extension stores the non residue elmt for an extension of type Fp->Fp2->Fp6->Fp12 (Fp2 = Fp(u), Fp6 = Fp2(v), Fp12 = Fp6(w))
