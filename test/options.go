84897ec65a529d76fd067ce5aebd949d87353ed7
xingzwh <90246774+xingzwh@users.noreply.github.com>
2023-04-28 10:57:48 +0800

    Update options.go

--- test/options.go
+++ test/options.go
@@ -18,8 +18,8 @@ package test
 
 import (
 	"github.com/consensys/gnark-crypto/ecc"
-	"github.com/consensys/gnark/backend"
-	"github.com/consensys/gnark/frontend"
+	"github.com/xingzwh/gnark/backend"
+	"github.com/xingzwh/gnark/frontend"
 )
 
 // TestingOption defines option for altering the behaviour of Assert methods.
